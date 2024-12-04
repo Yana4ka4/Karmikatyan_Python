c5c59367ad4fd5218688a4e5e7c97cdb10503ebc
Yana4ka4 <yyanakarmikatian@gmail.com>
2024-12-04 21:15:37 +0300

    fix

--- ПЗ_6/PZ_6_2.py
+++ ПЗ_6/PZ_6_2.py
@@ -1,18 +1,18 @@
 # Вариант 12. Дан список размера N. Найти два соседних элемента, сумма которых максимальна,
 # и вывести эти элементы в порядке возрастания их индексов.
 
-def pairSum(data):
-    if len(data) < 2:
+def pairSum(numbers):
+    if len(numbers) < 2:
        return None
 
     maxSum = 0
     maxPair = None
 
-    for i in range(len(data) - 1):
-        tekushayaSum = data[i] + data[i + 1]
+    for i in range(len(numbers) - 1):
+        tekushayaSum = numbers[i] + numbers[i + 1]
         if tekushayaSum > maxSum:
             maxSum = tekushayaSum
-            maxPair = (data[i], data[i + 1])
+            maxPair = (numbers[i], numbers[i + 1])
     return maxPair
 import random
 

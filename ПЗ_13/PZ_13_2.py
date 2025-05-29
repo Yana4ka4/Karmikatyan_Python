797a8c5a15164e5a42b309bf9df15ef8f32b758f
Yana4ka4 <yyanakarmikatian@gmail.com>
2025-05-29 20:56:31 +0300

    fix

--- ПЗ_13/PZ_13_2.py
+++ ПЗ_13/PZ_13_2.py
@@ -1,15 +1,26 @@
 # Вариант 12. В двумерном списке найти отрицательные элементы, сформировать из них новый
 # массив. Вывести размер полученного массива.
 
-spisok = [
-    [1, -2, 3],
-    [-4, 5, -6],
-    [7, -8, 9]
-]
+import random
+stolbec = int(input('Введите количество столбцов: '))
+stroka = int(input('Введите количество строк: '))
 
+spisok = [[random.randint(0,10) for el in range(stolbec)] for el in range(stroka)]
 negatives = list(filter(lambda x: x < 0, [num for stroka in spisok for num in stroka]))
+
+print('Исходная матрица: ')
+for s in spisok:
+    print(s)
 
 print(negatives)
 print("Размер:", len(negatives))
 
 
+
+
+
+# spisok = [
+#     [1, -2, 3],
+#     [-4, 5, -6],
+#     [7, -8, 9]
+# ]
